cad33f492fab7ae9ac765efeb442a328ddbee5fc
Alice Freda <alicefreda@gmail.com>
2019-11-11 22:12:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,8 +18,17 @@ class Scraper
     #end
   end
 
-  def get_courses
-  self.get_page.css(".post")
+  def make_courses
+  self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
 end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
 
 end
